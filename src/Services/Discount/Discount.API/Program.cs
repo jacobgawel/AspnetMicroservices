c6ca4caf0cb53cbf6fad10f129405ae59ae1f234
Jakub Gawel <jakubgawel@icloud.com>
2023-09-06 20:37:55 +0100

    Added seeding middleware to the Discount.API startup

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,9 +1,11 @@
+using Discount.API.Extensions;
 using Discount.API.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +17,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // adding middleware to the program. e.g. this will method will get executed
+    // when the program is launched The <Program> part suggests that it's a generic method where Program is a type parameter.
+    app.MigrateDatabase<Program>();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
